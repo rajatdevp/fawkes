1c83f384ba7057a6e18a440350ad66037404dffa
Rajat Kumar <rajatsrajesh@gmail.com>
2025-08-03 15:01:02 +0530

    init

---
+++ src/main/java/io/host/fawkes/FawkesApplication.java
@@ -0,0 +1,13 @@
+package io.host.fawkes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawkesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FawkesApplication.class, args);
+    }
+
+}
